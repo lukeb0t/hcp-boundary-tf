e1a0f1eebb98924936f8505c7ebdf44d4fb7fd4e
Luke McCleary <luke.mccleary@gmail.com>
2023-05-05 12:31:52 -0400

    awd

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ provider "aws" {
 }
 
 data "aws_key_pair" "user_keypair" {
-  count = "${var.keypair_name != "" ? 1 : 0}"
+  count = "${var.keypair_name != "" ? 0 : 1}"
   key_name = var.keypair_name
   include_public_key = true
 }
