5529c5179888def673a7ebecd640d5a30b03cce1
Luke McCleary <luke.mccleary@gmail.com>
2023-05-05 10:51:04 -0400

    continue unit testing

--- main.tf
+++ main.tf
@@ -83,64 +83,64 @@ module "aws_infra" {
   igw_id = var.igw_id
 }
 
-# module "boundary_worker" {
-#   depends_on = [ module.aws_infra ]
-#   source = "./boundary_worker"
-#   unique_name = local.unique_name
-#   aws_region = var.aws_region
-#   vpc_id = var.vpc_id
-#   aws_ami = module.aws_infra.aws_ami_ubuntu
-#   aws_public_secgroup_id = module.aws_infra.aws_secgroup_public_id
-#   app_infra_ssh_privkey = module.aws_infra.app_infra_ssh_privkey
-#   boundary_worker_instance_type = var.aws_boundary_worker_instance_type
-#   boundary_worker_subnet_id = module.aws_infra.aws_subnet_public_id
-#   boundary_cluster_admin_url = var.boundary_cluster_admin_url
-# }
-# 
-# module "postgres" {
-#   depends_on = [ module.aws_infra ]
-#   source = "./postgres"
-#   unique_name = local.unique_name
-#   aws_region = var.aws_region
-#   aws_ami = module.aws_infra.aws_ami_ubuntu
-#   pg_instance_type = var.aws_postgres_node_instance_type
-#   pg_subnet_id = module.aws_infra.aws_subnet_private_id
-#   pg_secgroup_id = module.aws_infra.aws_secgroup_private_id
-#   pg_ssh_keypair = module.aws_infra.aws_ssh_keypair_app_infra
-# }
-# 
-# module "k8s_cluster" {
-#   depends_on = [ module.aws_infra, module.boundary_worker ]
-#   source = "./k8s_cluster"
-#   unique_name = local.unique_name
-#   aws_region = var.aws_region
-#   vpc_id = var.vpc_id
-#   aws_ami = module.aws_infra.aws_ami_ubuntu
-#   boundary_cluster_admin_url = var.boundary_cluster_admin_url
-#   boundary_instance_worker_addr = "${module.boundary_worker.boundary_worker_dns_public}:9202"
-#   k8s_instance_type = var.aws_k8s_node_instance_type
-#   k8s_subnet_id = module.aws_infra.aws_subnet_private_id
-#   k8s_secgroup_id = module.aws_infra.aws_secgroup_private_id
-#   k8s_boundary_worker_lb_subnet_id = module.aws_infra.aws_subnet_public_id
-#   k8s_boundary_worker_lb_secgroup_id = module.aws_infra.aws_secgroup_public_id
-#   k8s_ssh_keypair = module.aws_infra.aws_ssh_keypair_app_infra
-#   k8s_nodeport_lb_vpc = var.vpc_id
-# }
-# 
-# module "vault_server" {
-#   depends_on = [ module.postgres, module.k8s_cluster ]
-#   source = "./vault_server"
-#   unique_name = local.unique_name
-#   aws_region = var.aws_region
-#   aws_ami = module.aws_infra.aws_ami_ubuntu
-#   vault_instance_type = var.aws_vault_node_instance_type
-#   vault_subnet_id = module.aws_infra.aws_subnet_private_id
-#   vault_secgroup_id = module.aws_infra.aws_secgroup_private_id
-#   vault_ssh_keypair = module.aws_infra.aws_ssh_keypair_app_infra
-#   vault_lb_vpc = var.vpc_id
-#   create_postgres = var.create_postgres
-#   postgres_server = module.postgres.dns
-#   pg_vault_user = module.postgres.vault_user
-#   pg_vault_password = module.postgres.vault_password
-# }
+module "boundary_worker" {
+  depends_on = [ module.aws_infra ]
+  source = "./boundary_worker"
+  unique_name = local.unique_name
+  aws_region = var.aws_region
+  vpc_id = var.vpc_id
+  aws_ami = module.aws_infra.aws_ami_ubuntu
+  aws_public_secgroup_id = module.aws_infra.aws_secgroup_public_id
+  app_infra_ssh_privkey = module.aws_infra.app_infra_ssh_privkey
+  boundary_worker_instance_type = var.aws_boundary_worker_instance_type
+  boundary_worker_subnet_id = module.aws_infra.aws_subnet_public_id
+  boundary_cluster_admin_url = var.boundary_cluster_admin_url
+}
+
+module "postgres" {
+  depends_on = [ module.aws_infra ]
+  source = "./postgres"
+  unique_name = local.unique_name
+  aws_region = var.aws_region
+  aws_ami = module.aws_infra.aws_ami_ubuntu
+  pg_instance_type = var.aws_postgres_node_instance_type
+  pg_subnet_id = module.aws_infra.aws_subnet_private_id
+  pg_secgroup_id = module.aws_infra.aws_secgroup_private_id
+  pg_ssh_keypair = module.aws_infra.aws_ssh_keypair_app_infra
+}
+
+module "k8s_cluster" {
+  depends_on = [ module.aws_infra, module.boundary_worker ]
+  source = "./k8s_cluster"
+  unique_name = local.unique_name
+  aws_region = var.aws_region
+  vpc_id = var.vpc_id
+  aws_ami = module.aws_infra.aws_ami_ubuntu
+  boundary_cluster_admin_url = var.boundary_cluster_admin_url
+  boundary_instance_worker_addr = "${module.boundary_worker.boundary_worker_dns_public}:9202"
+  k8s_instance_type = var.aws_k8s_node_instance_type
+  k8s_subnet_id = module.aws_infra.aws_subnet_private_id
+  k8s_secgroup_id = module.aws_infra.aws_secgroup_private_id
+  k8s_boundary_worker_lb_subnet_id = module.aws_infra.aws_subnet_public_id
+  k8s_boundary_worker_lb_secgroup_id = module.aws_infra.aws_secgroup_public_id
+  k8s_ssh_keypair = module.aws_infra.aws_ssh_keypair_app_infra
+  k8s_nodeport_lb_vpc = var.vpc_id
+}
+
+module "vault_server" {
+  depends_on = [ module.postgres, module.k8s_cluster ]
+  source = "./vault_server"
+  unique_name = local.unique_name
+  aws_region = var.aws_region
+  aws_ami = module.aws_infra.aws_ami_ubuntu
+  vault_instance_type = var.aws_vault_node_instance_type
+  vault_subnet_id = module.aws_infra.aws_subnet_private_id
+  vault_secgroup_id = module.aws_infra.aws_secgroup_private_id
+  vault_ssh_keypair = module.aws_infra.aws_ssh_keypair_app_infra
+  vault_lb_vpc = var.vpc_id
+  create_postgres = var.create_postgres
+  postgres_server = module.postgres.dns
+  pg_vault_user = module.postgres.vault_user
+  pg_vault_password = module.postgres.vault_password
+}
 
